fec6ee0c1aa749c18425ee5c7aa9029f28c26a13
Pravin Sonawane <june.pravin@gmail.com>
2022-05-08 14:20:17 -0700

    Created compose Bar function
    - a simple compose function that displays a Bar
    - by default, the function uses all available size (width and height) with `fillMaxSize()`
    - currently, uses `Color.Blue` to render the bar. This will be made customizable in the future.

---
+++ lib/src/main/java/kplot/bar/Bar.kt
@@ -0,0 +1,31 @@
+package kplot.bar
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+/**
+ * A compose function that displays a bar.
+ * @param modifier a compose [Modifier]
+ */
+@Composable
+fun Bar(modifier: Modifier = Modifier) {
+    Box(modifier = modifier
+        .fillMaxSize()
+        .background(color = Color.Blue)) {
+
+    }
+}
+
+/**
+ * Compose preview for [Bar]
+ */
+@Preview
+@Composable
+fun BarPreview() {
+    Bar()
+}
