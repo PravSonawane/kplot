61cddf1d5ed2b4569d4c9f7345741d439a0c3813
Pravin Sonawane <june.pravin@gmail.com>
2022-05-08 20:50:04 -0700

    Created BarPlotData to hold BarPlot data
    Signed-off-by: Pravin Sonawane <june.pravin@gmail.com>

--- lib/src/main/java/kplot/bar/BarPlot.kt
+++ lib/src/main/java/kplot/bar/BarPlot.kt
@@ -5,17 +5,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Constraints
+import kplot.data.BarPlotData
 
 /**
  * A compose function that plots [Bar]s
  */
 @Suppress("MagicNumber")
 @Composable
-fun BarPlot(modifier: Modifier = Modifier) {
+fun BarPlot(barPlotData: BarPlotData, modifier: Modifier = Modifier) {
+    val maxData = barPlotData.values.maxOrNull() ?: 1f
     Layout(
         modifier = Modifier,
         content = {
-            repeat(5) {
+            repeat(barPlotData.values.size) {
                 Bar()
             }
         }
@@ -24,23 +26,27 @@ fun BarPlot(modifier: Modifier = Modifier) {
         val barCount = measurables.size
         val barMinThicknessPercentage = 0.01
         val barMaxThicknessPercentage = 0.05
-        val placeables = measurables.map {
-            it.measure(
+        val placeables = measurables.mapIndexed { i, p ->
+            p.measure(
                 Constraints(
                     minWidth = (constraints.minWidth * barMinThicknessPercentage).toInt(),
                     maxWidth = (constraints.maxWidth * barMaxThicknessPercentage).toInt(),
                     minHeight = constraints.minHeight,
-                    maxHeight = constraints.maxHeight
+                    maxHeight = (constraints.maxHeight * (barPlotData.values[i] / maxData)).toInt()
                 )
             )
         }
 
         layout(constraints.maxWidth, constraints.maxHeight) {
-            var xOffset = 0
+            var x = 0
             placeables.forEach {
-                val itemWidth = it.width / 2
-                xOffset += constraints.maxWidth / (barCount + 1)
-                it.place(xOffset - itemWidth, 0)
+                // mid point of the item width
+                val itemWidthMid = it.width / 2
+                // y coordinate where item needs to be placed
+                val y = constraints.maxHeight - it.height
+
+                x += constraints.maxWidth / (barCount + 1)
+                it.place(x - itemWidthMid, y)
             }
         }
     }
@@ -49,5 +55,7 @@ fun BarPlot(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 fun BarPlotPreview() {
-    BarPlot()
+    BarPlot(
+        barPlotData = BarPlotData(floatArrayOf(1f, 7f, 2f))
+    )
 }
