41773a80aedeb24f161585bba590c3e81319ee8c
Pravin Sonawane <june.pravin@gmail.com>
2022-05-11 20:58:25 -0700

    Update LinePlot color to primary color
    Signed-off-by: Pravin Sonawane <june.pravin@gmail.com>

--- lib/src/main/java/kplot/line/LinePlot.kt
+++ lib/src/main/java/kplot/line/LinePlot.kt
@@ -2,22 +2,23 @@ package kplot.line
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun LinePlot(data: FloatArray, modifier: Modifier = Modifier) {
 
+    val primaryColor = MaterialTheme.colors.primary
     Canvas(modifier = modifier.fillMaxSize()) {
         val canvasWidth = size.width
         val canvasHeight = size.height
         drawLine(
             start = Offset(x = canvasWidth, y = 0f),
             end = Offset(x = 0f, y = canvasHeight),
-            color = Color.Blue,
+            color = primaryColor,
             strokeWidth = 4f
         )
     }
