7ae257a929e3f5d9cfb3332673c5043e5605fd60
Pravin Sonawane <june.pravin@gmail.com>
2022-05-11 20:43:38 -0700

    Update Bar to have rounded corners at top
    Signed-off-by: Pravin Sonawane <june.pravin@gmail.com>

--- lib/src/main/java/kplot/bar/Bar.kt
+++ lib/src/main/java/kplot/bar/Bar.kt
@@ -3,11 +3,14 @@ package kplot.bar
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 /**
  * A compose function that displays a bar.
@@ -17,6 +20,7 @@ import androidx.compose.ui.tooling.preview.Preview
 fun Bar(modifier: Modifier = Modifier) {
     Box(modifier = modifier
         .fillMaxSize()
+        .clip(RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp))
         .background(color = MaterialTheme.colors.primary)) {
 
     }
