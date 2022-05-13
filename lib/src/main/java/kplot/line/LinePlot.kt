c80e62b676ae303ebf79b73e6be099c1b552962e
Pravin Sonawane <june.pravin@gmail.com>
2022-05-12 17:59:12 -0700

    LinePlot: add circles to smooth off line ends
    Signed-off-by: Pravin Sonawane <june.pravin@gmail.com>

--- lib/src/main/java/kplot/line/LinePlot.kt
+++ lib/src/main/java/kplot/line/LinePlot.kt
@@ -7,12 +7,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun LinePlot(data: FloatArray, modifier: Modifier = Modifier) {
+fun LinePlot(
+    data: FloatArray,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colors.primary,
+    width: Dp = 2.dp
+) {
     val maxData = remember { data.maxOrNull() ?: 1f }
-    val primaryColor = MaterialTheme.colors.primary
 
     Canvas(modifier = modifier.fillMaxSize()) {
         val maxWidth = size.width
@@ -23,12 +30,22 @@ fun LinePlot(data: FloatArray, modifier: Modifier = Modifier) {
             val yStart = maxHeight - (maxHeight * (data[it] / maxData))
             val xEnd = maxWidth * ((it + 1f) / (data.size - 1))
             val yEnd = maxHeight - (maxHeight * (data[it + 1] / maxData))
+            val start = Offset(x = xStart, y = yStart)
+            val end = Offset(x = xEnd, y = yEnd)
+
             drawLine(
-                start = Offset(x = xStart, y = yStart),
-                end = Offset(x = xEnd, y = yEnd),
-                color = primaryColor,
-                strokeWidth = 4f
+                start = start,
+                end = end,
+                color = color,
+                strokeWidth = width.toPx()
             )
+
+            // add circles to smooth off line ends
+            if(it == 0) {
+                drawCircle(color = color, center = start, radius = width.toPx() / 2)
+            }
+            drawCircle(color = color, center = end, radius = width.toPx() / 2)
+
         }
     }
 }
