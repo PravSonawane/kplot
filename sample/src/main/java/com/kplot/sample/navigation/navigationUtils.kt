016c7ea3ccdade08ec7b7ab04f92a6daa3e721ae
Pravin Sonawane <june.pravin@gmail.com>
2022-05-14 15:38:00 -0700

    Refactor navigation utils
    Signed-off-by: Pravin Sonawane <june.pravin@gmail.com>

--- sample/src/main/java/com/kplot/sample/navigation/navigationUtils.kt
+++ sample/src/main/java/com/kplot/sample/navigation/navigationUtils.kt
@@ -8,22 +8,29 @@ import com.kplot.sample.livedata.LiveDataWrapper
 fun Fragment.navigate(to: LiveDataWrapper<NavigationEvent>) {
     val content = to.getContentIfNotHandled() ?: return
 
-    if (content is NavigationEvent.NavigateTo) {
-        when (content.dest) {
-            Destination.MAIN -> {
-                findNavController().navigate(R.id.main_fragment)
-            }
-            Destination.CUSTOM -> {
-                findNavController().navigate(R.id.custom_fragment)
-            }
-            Destination.BAR -> {
-                findNavController().navigate(R.id.bar_fragment)
-            }
-            Destination.LINE -> {
-                findNavController().navigate(R.id.line_fragment)
-            }
-        }
-    } else if (content is NavigationEvent.OnBack) {
-        findNavController().popBackStack()
+    when (content) {
+        is NavigationEvent.NavigateTo -> navigateTo(content)
+        is NavigationEvent.OnBack -> navigateBack()
     }
+}
+
+private fun Fragment.navigateTo(content: NavigationEvent.NavigateTo) {
+    when (content.dest) {
+        Destination.MAIN -> {
+            findNavController().navigate(R.id.main_fragment)
+        }
+        Destination.CUSTOM -> {
+            findNavController().navigate(R.id.custom_fragment)
+        }
+        Destination.BAR -> {
+            findNavController().navigate(R.id.bar_fragment)
+        }
+        Destination.LINE -> {
+            findNavController().navigate(R.id.line_fragment)
+        }
+    }
+}
+
+private fun Fragment.navigateBack() {
+    findNavController().popBackStack()
 }
